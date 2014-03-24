067f30945397ad29d70340b6ec5df150b608a317
nrandall92 <nrandall@mail.umw.edu>
2014-03-24 16:14:32 -0400

    added query for shakespeare insults

--- server.py
+++ server.py
@@ -73,6 +73,67 @@ def insult( ):
   adjective = cur.fetchall()
   return render_template('insult.html', verb = verb, noun = noun, adjective = adjective)
 
+@app.route('/insult2', methods=['POST','GET'])
+def insult2( ):
+  db = utils.db_connect()
+  cur = db.cursor()
+  
+  if request.method == 'POST':
+    #Get the intensity from the form
+    global intensity
+    intensity = request.form['intense']
+  
+  print intensity
+  #Getting the verb
+  #search based on intensity
+  query = "SELECT id FROM insult_verbs WHERE intensity = '" + str(intensity) + "'"
+  cur.execute(query)
+  #get all the verbs of that intensity
+  possible = cur.fetchall()
+  numpossible = len(possible)
+  #pick a random index to use
+  rand = random.randint(0,numpossible-1)
+  #get the value at that index
+  target = possible[rand][0]
+  #use that value as the id of the verb
+  query = "SELECT verb FROM insult_verbs WHERE id = " + str(target)
+  cur.execute(query)
+  verb = cur.fetchall()
+  
+  #Getting the noun
+  #search based on intensity
+  query = "SELECT id FROM insult_nouns WHERE intensity = '" + str(intensity) + "'"
+  cur.execute(query)
+  #get all the verbs of that intensity
+  possible = cur.fetchall()
+  numpossible = len(possible)
+  #pick a random index to use
+  rand = random.randint(0,numpossible-1)
+  #get the value at that index
+  target = possible[rand][0]
+  #use that value as the id of the verb
+  query = "SELECT noun FROM insult_nouns WHERE id = " + str(target)
+  cur.execute(query)
+  noun = cur.fetchall()
+  
+  #Getting the adjective
+  #search based on intensity
+  query = "SELECT id FROM insult_adjectives WHERE intensity = '" + str(intensity) + "'"
+  cur.execute(query)
+  #get all the verbs of that intensity
+  possible = cur.fetchall()
+  numpossible = len(possible)
+  #pick a random index to use
+  rand = random.randint(0,numpossible-1)
+  #get the value at that index
+  target = possible[rand][0]
+  #use that value as the id of the verb
+  query = "SELECT adjective FROM insult_adjectives WHERE id = " + str(target)
+  cur.execute(query)
+  adjective = cur.fetchall()
+  return render_template('insult2.html', verb = verb, noun = noun, adjective = adjective)
+
+
 @app.route('/contact.html')
 def contact( ):
   return render_template('contact.html')
