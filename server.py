031147adce584e882b700df1658f710ca06ef245
dlustig <dllustig@gmail.com>
2014-03-31 19:57:21 -0400

    Update server.py

--- server.py
+++ server.py
@@ -115,6 +115,105 @@ def shakespeare( ):
   adjective1 = cur.fetchall()
   return render_template('shakespeare.html', verb1 = verb1, noun1 = noun1, adjective1 = adjective1)
 
+
+
+
+@app.route('/responses', methods=['POST','GET'])
+def responses( ):
+  db = utils.db_connect()
+  cur = db.cursor()
+  
+  #/***************************/
+  #/****# shakespeare insults *****/
+  
+  query = "SELECT COUNT(*) FROM shakespeare_verbs"
+  cur.execute(query)
+  numverbs = cur.fetchall()
+  rand = random.randint(1,numverbs[0][0])
+  query = "SELECT verb FROM shakespeare_verbs WHERE id = " + str(rand)
+  cur.execute(query)
+  verb1 = cur.fetchall()
+  
+  query = "SELECT COUNT(*) FROM shakespeare_nouns"
+  cur.execute(query)
+  numnouns = cur.fetchall()
+  rand = random.randint(1,numnouns[0][0])
+  query = "SELECT noun FROM shakespeare_nouns WHERE id = " + str(rand)
+  cur.execute(query)
+  noun1 = cur.fetchall()
+  
+  query = "SELECT COUNT(*) FROM shakespeare_adjectives"
+  cur.execute(query)
+  numadjectives = cur.fetchall()
+  rand = random.randint(1,numadjectives[0][0])
+  query = "SELECT adjective FROM shakespeare_adjectives WHERE id = " + str(rand)
+  cur.execute(query)
+  adjective1 = cur.fetchall()
+  
+  #/***************************/
+  #/****# regular insults *****/
+  
+
+  query = "SELECT id FROM insult_verbs"
+  cur.execute(query)
+  #get all the verbs of that intensity
+  possible = cur.fetchall()
+  numpossible = len(possible)
+  #pick a random index to use
+  rand = random.randint(0,numpossible-1)
+  #get the value at that index
+  target = possible[rand][0]
+  #use that value as the id of the verb
+  query = "SELECT verb FROM insult_verbs WHERE id = " + str(target)
+  cur.execute(query)
+  verb = cur.fetchall()
+  
+  #Getting the noun
+  #search based on intensity
+  query = "SELECT id FROM insult_nouns"
+  cur.execute(query)
+  #get all the verbs of that intensity
+  possible = cur.fetchall()
+  numpossible = len(possible)
+  #pick a random index to use
+  rand = random.randint(0,numpossible-1)
+  #get the value at that index
+  target = possible[rand][0]
+  #use that value as the id of the verb
+  query = "SELECT noun FROM insult_nouns WHERE id = " + str(target)
+  cur.execute(query)
+  noun = cur.fetchall()
+  
+  #Getting the adjective
+  #search based on intensity
+  query = "SELECT id FROM insult_adjectives"
+  cur.execute(query)
+  #get all the verbs of that intensity
+  possible = cur.fetchall()
+  numpossible = len(possible)
+  #pick a random index to use
+  rand = random.randint(0,numpossible-1)
+  #get the value at that index
+  target = possible[rand][0]
+  #use that value as the id of the verb
+  query = "SELECT adjective FROM insult_adjectives WHERE id = " + str(target)
+  cur.execute(query)
+  adjective = cur.fetchall()
+  
+  #/***************************/
+  #/******# long insult *******/
+
+  query = "SELECT fi.insult FROM full_insults AS fi INNER JOIN shakespeare_adjectives AS sa ON fi.foreign_key = sn.id AND sa.id = " + str(target)
+  cur.execute(query)
+  fullInsult = cur.fetchall()
+  
+  
+  #/**************************/
+  
+  return render_template('responses.html', verb1 = verb1, noun1 = noun1, adjective1 = adjective1, verb = verb, noun = noun, adjective = adjective, fullInsult = fullInsult)
+
+
+
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     global currentUser
